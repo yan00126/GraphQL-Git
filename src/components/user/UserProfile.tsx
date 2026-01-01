2c93634dd50cef4eebd3635e46e2726d2cc231a9
yan fei <yan00126@algonquinlive.com>
2026-01-01 11:35:05 -0500

    update userprofile,apparently correctly fetching data from github

--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -1,9 +1,32 @@
+import { useQuery } from "@apollo/client";
+import { GET_USER } from "@/queries";
+import { UserData } from "@/types";
+
 type UserProfileProps = {
   userName: string;
 };
 
 const UserProfile = ({ userName }: UserProfileProps) => {
-  return <h1 className="text-2xl font-bold">{userName}</h1>;
+  const { data, loading, error } = useQuery<UserData>(GET_USER, {
+    variables: { login: userName },
+  });
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <h2 className="text-xl">{error.message}</h2>;
+
+  if (!data) return <h2 className="text-xl">User Not Found</h2>;
+
+  const {
+    avatarUrl,
+    name,
+    bio,
+    url,
+    repositories,
+    followers,
+    following,
+    gists,
+  } = data.user;
+  return <h1 className="text-2xl font-bold">{avatarUrl}</h1>;
 };
 
 export default UserProfile;
